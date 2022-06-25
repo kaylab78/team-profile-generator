3c077808ecd01a80e9988f8ae9015d69c2e83cb9
kaylab78 <kaylabackus78@gmail.com>
2022-06-25 12:42:53 -0500

    Override 'Employee' role with 'Engineer'

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -14,4 +14,11 @@ test("gets engineer's GitHub username", () => {
     const engineer = new Engineer('Kevin', 6, 'email@email.com', 'lernantino');
 
     expect(engineer.getGitHub()).toEqual(expect.stringContaining(engineer.github));
+});
+
+test("get engineer's role", () => {
+    const engineer = new Engineer('Kevin', 6, 'email@email.com', 'lernantino');
+
+    // Overrides inherited role of 'Employee' with 'Engineer'
+    expect(engineer.getRole()).toEqual(expect.stringContaining('Engineer'));
 })
