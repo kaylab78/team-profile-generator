393f9967dd2b3b243a1713b6b1c95ec568b307bb
kaylab78 <kaylabackus78@gmail.com>
2022-06-25 12:24:26 -0500

    Edit Engineer github username

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -8,4 +8,5 @@ test('creates new engineer object', () => {
     expect(engineer.id).toEqual(expect.any(Number));
     expect(engineer.email).toEqual(expect.any(String));
     expect(engineer.github).toEqual(expect.any(String));
-})
+});
+
