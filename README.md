fe38a914f8835aba5f5a879aea1b1237fd8fa283
kaylab78 <kaylabackus78@gmail.com>
2022-06-25 09:16:25 -0500

    First test for Employee object

--- README.md
+++ README.md
@@ -24,6 +24,8 @@ This project is licensed under the MIT license.
 [License: MIT License](https://opensource.org/licenses/MIT)
 
 ## Credits
+Wallace, Nicole. (2020, May 17). Team-profile-generator. GitHub. [https://github.com/nicolewallace09/team-profile-generator/blob/master/__tests__/Employee.test.js](https://github.com/nicolewallace09/team-profile-generator/blob/master/__tests__/Employee.test.js).
+
 [Inquirer.js](https://www.npmjs.com/package/inquirer)
 [Jest.js](https://www.npmjs.com/package/jest)
 [Node.js](https://nodejs.org/en/)
--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,18 @@
 const Employee = require('../lib/Employee.js');
 
 test('creates a new employee object', () => {
-    const employee = new Employee('Kevin');
+    const employee = new Employee('Kevin', 6, 'email@email.com');
 
-    expect(employee.name).toBe('Kevin');
-})
+    // check to see if object is created
+    // reference Wallace, Nicole.
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+    // end reference
+});
+
+// test("gets employee's name", () => {
+//     const employee = new Employee('Kevin');
+
+//     expect(employee.getName()).toBe('Kevin');
+// })
